1e45a59f71428542452d953520386c172a9fc999
Armando Andini <armando.andini@hotmail.com>
2016-08-25 21:43:32 -0300

    Agregamos logica al evento del selector de dias.

--- app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/isi/dam/MainActivity.java
@@ -2,6 +2,8 @@ package ar.edu.utn.frsf.isi.dam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindSeekBar();
 
+
+
+    }
+
+    private void bindSeekBar(){
+        SeekBar selectorDias = (SeekBar) findViewById(R.id.selectorDias);
+        selectorDias.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                TextView textViewDias = (TextView)findViewById(R.id.cantidadDias);
+                String dias = String.valueOf(progress);
+                textViewDias.setText(dias);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
